b6d1c9980bea50239e02b10abc76307692cfc68f
CameronLumpkinATL <camlump@yahoo.com>
2020-02-23 22:21:55 -0500

    created heros model

---
+++ models/hero.js
@@ -0,0 +1,53 @@
+const mongoose = require('../db/connection.js')
+
+
+const heroes = new mongoose.Schema({
+    gender: {
+        type: String,
+        
+        required: true,
+
+        enum: ['male', 'female', 'unkown'],
+
+    },
+
+    teacher: {
+        type: String,
+
+        required: true,
+
+        enum: [ 'yes', 'No'],
+    },
+
+
+    heroStatus: {
+        type: String,
+
+        required: true,
+
+        enum: [ 'Active', 'Active', 'Former'],
+    },
+
+    overview: {
+        type: String,
+        required: true,
+    },
+
+    quirk: {
+        type: String,
+
+        required: true,
+    },
+
+    quirkDescription: {
+        type: String,
+        
+        required: true,
+    },
+
+
+
+})
+
+
+module.exports = mongoose('heroes', heroes)
