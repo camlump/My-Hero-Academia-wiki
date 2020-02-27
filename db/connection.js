aabb276c30f982ed3b73cc5067011841b5bf9ad9
CameronLumpkinATL <camlump@yahoo.com>
2020-02-27 09:52:17 -0500

    fixed stuedent form, lowercase gender

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,29 @@
 
 const mongoose = require('mongoose');
 
-const connectiontring = process.env.MONGODB_URI || "mongodb://localhost/my-hero-wiki";
+// const connectiontring = process.env.MONGODB_URI || "mongodb://localhost/my-hero-wiki";
 
-//open connection to the mongo database
+// //open connection to the mongo database
 
-mongoose.connect(connectiontring, { useNewUrlParser: true})
-    .then(()=>{
-        console.log("connected to mongo at: " + connectiontring);
+// mongoose.connect(connectiontring, { useNewUrlParser: true})
+//     .then(()=>{
+//         console.log("connected to mongo at: " + connectiontring);
 
-    });
+//     });
+
 //Export the mongoose object
-
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/my-hero-wiki');
+}
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', () => {
+  console.log("Mongoose has connected to MongoDB!");
+});
 module.exports = mongoose
--- models/Students.js
+++ models/Students.js
@@ -12,7 +12,7 @@ const students = new mongoose.Schema({
 
     gender: {
         type: String,
-        required:true,
+        required: true,
         
         enum: ['Male', 'Female', 'unknown'],
     },
